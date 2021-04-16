89a5857d99decbd68706d47236f6cfe43dd8c0c8
Vik <jzenterprize90@gmail.com>
2021-04-15 23:05:16 -0400

    Added method with validation for numberical input

--- SimpleGrading/Program.cs
+++ SimpleGrading/Program.cs
@@ -8,6 +8,7 @@ namespace SimpleGrading
         {
             GradingWithIfElseBlock();
             GradingMinusIfElseBlock();
+            GradingMinusIfElseBlockWithIntValidation();
         }
         public static void GradingWithIfElseBlock()
         {
@@ -73,12 +74,64 @@ namespace SimpleGrading
             int scoreTotal = 0;
             int testScore = 0;
             int averageScore = 0;
-            Console.WriteLine("AltGradingConverted");
 
 
             Console.Write("Enter class size: ");
             int classSizeRatio = int.Parse(Console.ReadLine()) * 100;
             Console.WriteLine($"while test_score != {classSizeRatio}");
+            while (testScore != classSizeRatio)
+            {
+
+                Console.Write("Enter test score ");
+                testScore = int.Parse(Console.ReadLine());
+
+                string gradeLetter = (testScore >= 90) ? "A" :
+                (testScore >= 80) ? "B" :
+                (testScore >= 70) ? "C" :
+                (testScore >= 60) ? "D" :
+                "E";
+
+
+                if (testScore >= 0 && testScore <= 100)
+                {
+                    scoreTotal += testScore;
+                    counter += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Discarding score");
+                    Console.WriteLine($"Total score: {scoreTotal} ");
+                    continue;
+                }
+                if (scoreTotal >= classSizeRatio)
+                {
+                    Console.WriteLine("Scoring complete");
+                    Console.WriteLine($"Total score: {classSizeRatio} \nAverage score: {averageScore} \nGrade: {gradeLetter}");
+                    break;
+                }
+                averageScore = scoreTotal / counter;
+                Console.WriteLine($"Total score: {scoreTotal} \nAverage score: {averageScore} \nGrade: {gradeLetter}");
+            }
+        } // while (!int.TryParse(Console.ReadLine(), out classSizeRatio))
+        public static void GradingMinusIfElseBlockWithIntValidation()
+        {
+            int counter = 0;
+            int scoreTotal = 0;
+            int testScore = 0;
+            int averageScore = 0;
+            int classSizeRatio = 0;
+
+
+            Console.Write("Enter class size: ");
+            while (!int.TryParse(Console.ReadLine(), out classSizeRatio))
+            {
+                Console.WriteLine("Please enter a valid number");
+                Console.Write("Enter class size: ");
+            }
+
+            classSizeRatio *= 100;
+            Console.WriteLine($"while test_score != {classSizeRatio}");
+
             while (testScore != classSizeRatio)
             {
 
